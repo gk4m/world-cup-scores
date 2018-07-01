944bd6e9fc0f308ac95651a4b4fcd39a9cd971da
Grzegorz Kaminski <kaminski_grzegorz@wp.pl>
2018-07-01 19:08:08 +0200

    Add match penalties info

--- src/components/MatchItem/index.vue
+++ src/components/MatchItem/index.vue
@@ -1,45 +1,50 @@
 <template>
   <div :class="elClasses">
-    <time class="match-item__time">{{match.datetime | moment("H:mm")}}</time>
+    <div class="match-item__inner">
+      <time class="match-item__time">{{match.datetime | moment("H:mm")}}</time>
 
-    <div class="match-item__competitor match-item__competitor--left">
-      <template v-if="match.home_team.code === 'TBD'">
-        {{match.home_team.team_tbd}}
-      </template>
-      <template v-else>
-        <span>{{match.home_team.country}}</span>
-        <img :src="require(`@/assets/img/flags/${match.home_team.code.toLowerCase()}.png`)" alt=""/>
-      </template>
-    </div>
-
-    <span class="match-item__state">
-      <span class="match-item__result">
-        <template v-if="match.status !== 'future'">
-          {{match.home_team.goals}} : {{match.away_team.goals}}
+      <div class="match-item__competitor match-item__competitor--left">
+        <template v-if="match.home_team.code === 'TBD'">
+          {{match.home_team.team_tbd}}
         </template>
-        <template v-else> vs </template>
-      </span>
-    </span>
+        <template v-else>
+          <span>{{match.home_team.country}}</span>
+          <img :src="require(`@/assets/img/flags/${match.home_team.code.toLowerCase()}.png`)" alt=""/>
+        </template>
+      </div>
 
-    <div class="match-item__competitor match-item__competitor--right">
-      <template v-if="match.away_team.code === 'TBD'">
-        {{match.away_team.team_tbd}}
-      </template>
-      <template v-else>
-        <img :src="require(`@/assets/img/flags/${match.away_team.code.toLowerCase()}.png`)" alt=""/>
-        <span>{{match.away_team.country}}</span>
-      </template>
+      <span class="match-item__state">
+        <span class="match-item__result">
+          <template v-if="match.status !== 'future'">
+            {{match.home_team.goals}} : {{match.away_team.goals}}
+          </template>
+          <template v-else> vs </template>
+        </span>
+      </span>
+
+      <div class="match-item__competitor match-item__competitor--right">
+        <template v-if="match.away_team.code === 'TBD'">
+          {{match.away_team.team_tbd}}
+        </template>
+        <template v-else>
+          <img :src="require(`@/assets/img/flags/${match.away_team.code.toLowerCase()}.png`)" alt=""/>
+          <span>{{match.away_team.country}}</span>
+        </template>
+      </div>
+
+      <span class="match-item__progress">
+        {{match.time}}
+      </span>
+
+      <button class="match-item__angle">
+        <icon class="fa-angle-down" name="angle-down"></icon>
+        <icon class="fa-angle-up" name="angle-up"></icon>
+      </button>
     </div>
 
-    <span class="match-item__progress">
-      {{match.time}}
-    </span>
-
-    <button class="match-item__angle">
-      <icon class="fa-angle-down" name="angle-down"></icon>
-      <icon class="fa-angle-up" name="angle-up"></icon>
-    </button>
-
+    <div class="match-item__penalties" v-if="match.home_team.penalties && match.away_team.penalties">
+      {{match.winner}} win on penalties ({{match.home_team.penalties}}-{{match.away_team.penalties}})
+    </div>
   </div>
 </template>
 
@@ -69,9 +74,6 @@
 <style lang="sass" scoped>
   .match-item
     position: relative
-    display: flex
-    justify-content: center
-    align-items: center
     padding: 4px 0
     border-top: 1px solid $c-gray-light
     font-size: 12px
@@ -109,8 +111,13 @@
 
     &--in-progress
       .match-item__progress
-        display: flex
-        align-items: center
+      display: flex
+      align-items: center
+
+    &__inner
+      display: flex
+      justify-content: center
+      align-items: center
 
     &__time
       position: absolute
@@ -163,6 +170,9 @@
       .fa-angle-up
         display: none
 
+    &__penalties
+      font-size: 10px
+
   +breakpoint(medium)
     .match-item
       padding: 7px 0
@@ -179,8 +189,12 @@
         display: inline-block
         width: 35px
 
-  @keyframes blink
-    50%
-      opacity: 0
+      &__penalties
+        margin: 3px 0 0
+        font-size: 12px
+
+    @keyframes blink
+      50%
+        opacity: 0
 
 </style>
